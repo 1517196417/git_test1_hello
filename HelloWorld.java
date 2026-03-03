c40356ca66d3398e6a83d617c0a006a5fb58ef35
1517196417 <1517196417@qq.com>
2026-03-03 19:20:01 +0800

    workspace2第1修改了HelloWorld文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 pubic class HelloWorld {
     public static void main (String[] args) {
 	    System.out.println("workspace1第二次修改了HelloWorld文件");
+	    System.out.println("workspace2第一次修改了HelloWorld文件,直接输出你好世界")
 }
 }
